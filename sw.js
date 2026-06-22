44a898b8731934df87947426ad4bc6909fc5834f
alxire2 <a_wilcox@hotmail.com>
2026-06-22 14:58:05 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'workout-v1';
+const CACHE_NAME = 'workout-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -23,6 +23,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
